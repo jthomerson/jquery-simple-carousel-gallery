jthomerson/jquery-simple-carousel-gallery

commit 77fcf2f6bd5d5b237d4b9367d310a913aeea35f0

Fix carousel centering bug
When the carousel was first centered it would often be off-center
because it was centering before the thumbnail had loaded.

dist/jquery.simple-carousel-gallery.js
@@ -155,14 +155,17 @@
       _scrollToSelectedThumbnail: function() {
          var carousel = this.element.find('.carousel'),
              firstThumb = carousel.find('.carouselItem:first-child'),
-             thumb = carousel.find('.carouselItem.active'),
-             carouselWidth = carousel.width(),
-             thumbWidth = thumb.outerWidth(),
-             left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
+             activeThumb = carousel.find('.carouselItem.active');
 
-         firstThumb.animate({
-            marginLeft: left + 'px'
-         }, this.settings.carouselAnimationSpeed);
+         firstThumb.find('img').load(function() {
+            var carouselWidth = carousel.width(),
+                thumbWidth = activeThumb.outerWidth(),
+                left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
+
+            firstThumb.animate({
+               marginLeft: left + 'px'
+            }, this.settings.carouselAnimationSpeed);
+         }.bind(this));
       },
 
       _updateButtons: function() {

src/jquery.simple-carousel-gallery.js
@@ -147,14 +147,17 @@
       _scrollToSelectedThumbnail: function() {
          var carousel = this.element.find('.carousel'),
              firstThumb = carousel.find('.carouselItem:first-child'),
-             thumb = carousel.find('.carouselItem.active'),
-             carouselWidth = carousel.width(),
-             thumbWidth = thumb.outerWidth(),
-             left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
+             activeThumb = carousel.find('.carouselItem.active');
 
-         firstThumb.animate({
-            marginLeft: left + 'px'
-         }, this.settings.carouselAnimationSpeed);
+         firstThumb.find('img').load(function() {
+            var carouselWidth = carousel.width(),
+                thumbWidth = activeThumb.outerWidth(),
+                left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
+
+            firstThumb.animate({
+               marginLeft: left + 'px'
+            }, this.settings.carouselAnimationSpeed);
+         }.bind(this));
       },
 
       _updateButtons: function() {

commit 5e96ee83bf88175132ffa7abe1d1ed7272aca694

Fix carousel centering bug introduced in 77fcf2f
It centered correctly only the first time.

dist/jquery.simple-carousel-gallery.js
@@ -44,8 +44,12 @@
              gallery = this.template.clone();
 
          gallery.find('.media').empty();
+
          this._renderCarouselInto(gallery);
          this.element.empty().append(gallery.html());
+
+         $(window).resize(this._scrollToSelectedThumbnail.bind(this));
+         this.element.find('.carouselItem img').eq(0).load(this._scrollToSelectedThumbnail.bind(this));
 
          this.element.on('click', '.carouselItem', function(evt) {
             var i = $(evt.target).closest('.carouselItem').data('index');
@@ -57,8 +61,6 @@
 
          this.element.find('.goToPrevious').click(this.previous.bind(this));
          this.element.find('.goToNext').click(this.next.bind(this));
-
-         $(window).resize(this._scrollToSelectedThumbnail.bind(this));
       },
 
       _getPrevInd: function() {
@@ -165,17 +167,14 @@
       _scrollToSelectedThumbnail: function() {
          var carousel = this.element.find('.carousel'),
              firstThumb = carousel.find('.carouselItem:first-child'),
-             activeThumb = carousel.find('.carouselItem.active');
-
-         firstThumb.find('img').load(function() {
-            var carouselWidth = carousel.width(),
-                thumbWidth = activeThumb.outerWidth(),
-                left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
+             activeThumb = carousel.find('.carouselItem.active'),
+             carouselWidth = carousel.width(),
+             thumbWidth = activeThumb.outerWidth(),
+             left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
 
             firstThumb.animate({
                marginLeft: left + 'px'
             }, this.settings.carouselAnimationSpeed);
-         }.bind(this));
       },
 
       _updateButtons: function() {

src/jquery.simple-carousel-gallery.js
@@ -36,8 +36,12 @@
              gallery = this.template.clone();
 
          gallery.find('.media').empty();
+
          this._renderCarouselInto(gallery);
          this.element.empty().append(gallery.html());
+
+         $(window).resize(this._scrollToSelectedThumbnail.bind(this));
+         this.element.find('.carouselItem img').eq(0).load(this._scrollToSelectedThumbnail.bind(this));
 
          this.element.on('click', '.carouselItem', function(evt) {
             var i = $(evt.target).closest('.carouselItem').data('index');
@@ -49,8 +53,6 @@
 
          this.element.find('.goToPrevious').click(this.previous.bind(this));
          this.element.find('.goToNext').click(this.next.bind(this));
-
-         $(window).resize(this._scrollToSelectedThumbnail.bind(this));
       },
 
       _getPrevInd: function() {
@@ -157,17 +159,14 @@
       _scrollToSelectedThumbnail: function() {
          var carousel = this.element.find('.carousel'),
              firstThumb = carousel.find('.carouselItem:first-child'),
-             activeThumb = carousel.find('.carouselItem.active');
-
-         firstThumb.find('img').load(function() {
-            var carouselWidth = carousel.width(),
-                thumbWidth = activeThumb.outerWidth(),
-                left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
+             activeThumb = carousel.find('.carouselItem.active'),
+             carouselWidth = carousel.width(),
+             thumbWidth = activeThumb.outerWidth(),
+             left = ((carouselWidth / 2) - (thumbWidth / 2) - (thumbWidth * this._currentIndex));
 
             firstThumb.animate({
                marginLeft: left + 'px'
             }, this.settings.carouselAnimationSpeed);
-         }.bind(this));
       },
 
       _updateButtons: function() {